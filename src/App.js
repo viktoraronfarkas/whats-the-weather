644c6791b93beb1d3a2c2ef05b550f7c96c06833
Viktor Farkas <fv1ktor09@gmail.com>
2021-06-13 21:00:08 +0200

    css fixes, add weather icon

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import $ from 'jquery';
 import apiKey from './secret';
 import cityStyle from './City.module.scss';
+require('dotenv').config();
 
 const getWeatherData = (city) => {
   let result = {};
+  let api_key = process.env.API_KEY;
 
   $.ajax({
     type: 'GET',
-    url: `http://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=${apiKey}`,
+    url: `https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=${apiKey}`,
     dataType: 'json',
     async: false,
     success: (data) => {
@@ -58,22 +60,25 @@ const removeCity = (e) => {
 }
 
 const City = (props) => {
-  const name = props?.data?.list[0]?.name;
-  const temp = props?.data?.list[0]?.main?.temp;
-  const feelsLike = props?.data?.list[0]?.main?.feels_like;
-  const clouds = props?.data?.list[0]?.clouds?.all;
-  const pressure = props?.data?.list[0]?.main?.pressure;
-  const weatherDesc = props?.data?.list[0]?.weather[0].description;
+  const name = props.data.list ? props?.data?.list[0]?.name : '';
+  const temp = props.data.list ? props?.data?.list[0]?.main?.temp : '';
+  const feelsLike = props.data.list ? props?.data?.list[0]?.main?.feels_like : '';
+  const clouds = props.data.list ? props?.data?.list[0]?.clouds?.all : '';
+  const pressure = props.data.list ? props?.data?.list[0]?.main?.pressure : '';
+  const weatherDesc = props.data.list ? props?.data?.list[0]?.weather[0].description : '';
+  const iconCode = props.data.list ? props?.data?.list[0]?.weather[0].icon : '';
+  const iconSrc = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
   const style = cityStyle;
 
   return (
     <div className={style.container}>
       <h3 className={style.title}>{name}</h3>
+      <img className={style.weatherIcon} src={iconSrc} />
       <svg className={style.closeIcon} onClick={ removeCity } data-js-name={name} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times"
            role="img" xmlns="http://www.w3.org/2000/svg"
            viewBox="0 0 352 512">
         <path fill="currentColor" data-js-name={name}
-              d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
+              d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" />
       </svg>
       <h4>Current temperature: <span>{temp} °C</span></h4>
       <h4>Feels like: <span>{feelsLike} °C</span></h4>
